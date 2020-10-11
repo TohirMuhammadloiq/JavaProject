3cd5a5f854563b5bd328a40668c2573fbeafb9fc
Java_projects <tokhirkhuja6499@mail.ru>
2020-10-11 12:52:22 +0300

    It is register form in both side

---
+++ src/main/java/com/example/demo/Registr1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Registr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Registr1Application.class, args);
+	}
+
+}
